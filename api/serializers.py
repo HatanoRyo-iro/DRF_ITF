8a6c217e00c14f67a521000202999c3d48f24783
RyoHatano <ryohatano0809@gmail.com>
2023-05-29 09:38:05 +0900

    How to set initializer options.

--- api/serializers.py
+++ api/serializers.py
@@ -7,8 +7,17 @@ class BookSerializer(serializers.ModelSerializer):
     """
     本モデル用シリアライザ
     """
+    price = serializers.CharField(read_only=True)
     class Meta:
         # 対象のモデルクラスを指定
         model = Book
         # 利用するモデルのフィールドを指定
-        fields = ['id', 'title', 'price']
+        exclude = ['created_at']
+        extra_kwargs = {
+            'title' : {
+                'write_only' : True,   # 入力のみで利用
+                'max_length' : 10,   # バリデーションの最大桁数を10に変更
+            }
+        }
+        
+    
