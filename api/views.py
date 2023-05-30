95dabe39622b3c675cea2c83b47bc3259b67b618
RyoHatano <ryohatano0809@gmail.com>
2023-05-30 18:12:01 +0900

    Example of an Acquisition (List) API Class Inheriting from APIView

--- api/views.py
+++ api/views.py
@@ -1,7 +1,19 @@
+from django_filters import rest_framework as filters
 from rest_framework import status, views
+from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 
+from .models import Book
 from .serializers import BookSerializer
+
+
+class BookFilter(filters.FilterSet):
+    """
+    本モデル用フィルタクラス
+    """
+    class Meta:
+        model = Book
+        fields = '__all__'
 
 
 class BookCreateAPIView(views.APIView):
@@ -21,3 +33,23 @@ class BookCreateAPIView(views.APIView):
         serializer.save()
         # レスポンスオブジェクトを作成して返す
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+
+class BookListAPIView(views.APIView):
+    """
+    本モデルの取得（一覧）APIクラス
+    """
+    
+    def get(self, request, *args, **kwargs):
+        """
+        本モデルの取得（一覧）APIに対応するハンドラメソッド
+        """
+        # モデルオブジェクトをクエリ文字列を使ってフィルタリングした結果を取得
+        filterset = BookFilter(request.query_params, queryset=Book.objects.all())
+        if not filterset.is_valid():
+            # クエリ文字列のバリデーションがNGの場合は400エラー
+            raise ValidationError(filterset.errors)
+        # シリアライザオブジェクトを作成
+        serializer = BookSerializer(instance=filterset.qs, many=True)
+        # レスポンスオブジェクトを作成して返す
+        return Response(serializer.data)
