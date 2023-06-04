7935437a1c8a0d9f51e38d41e89a8b6a9283a487
RyoHatano <ryohatano0809@gmail.com>
2023-06-04 12:30:49 +0900

    Default DRF Settings

--- config/settings.py
+++ config/settings.py
@@ -130,6 +130,63 @@ REST_FRAMEWORK ={
 }
 
 DEFAULTS = {
+    # Base API policies
+    'DEFAULT_RENDERER_CLASSES' : [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
+    'DEFAULT_PARSER_CLASSES' : [
+        'rest_framework.parsers.JSONParser',
+        'rest_framework.parsers.FormParser',
+        'rest_framework.parsers.MultiPartParser',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES' : [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES' : [
+        'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_THROTTLE_CLASSES' : [],
+    'DEFAULT_CONTENT_NEGOTIATION_CLASS' : 'rest_framework.negotiation.DefaultContentNegotiation',
+    'DEFAULT_METADATA_CLASS' : 'rest_framework.metadata.SimpleMetadata',
+    'DEFAULT_VERSIONING_CLASS' : None,
+    
+    # Generic view behavior
+    'DEFAULT_PAGINATION_CLASS' : None,
+    'DEFAULT_FILTER_BACKENDS' : [],
+    
+    # Schema
+    'DEFAULT_SCHEMA_CLASS' : 'rest_framework.schemas.AutoSchema',
+    
+    # Throttling
+    'DEFAULT_THROTTLE_RATES' : {
+        'user' : None,
+        'anon' : None,
+    },
+    'NUM_PROXIES' : None,
+    
+    # Pagination
+    'PAGE_SIZE' : None,
+    
+    # Filtering
+    'SEARCH_PARAM' : 'search',
+    'ORDERING_PARAM' : 'ordering',
+    
+    # Versioning
+    'DEFAULT_VERSION' : None,
+    'ALLOWED_VERSIONS' : None,
+    'VERSION_PARAM' : 'version',
+    
+    # Authentication
+    'UNAUTHENTICATED_USER' : 'django.contrib.auth.models.AnonymousUser',
+    'UNAUTHENTICATED_TOKEN' : None,
+    
+    # View configuration
+    'VIEW_NAME_FUNCTION' : 'rest_framework.views.get_view_name',
+    'VIEW_DESCRIPTION_FUNCTION' : 'rest_framework.views.get_view_description',
+    
     # Exception handling
-    'EXCEPTION_HANDLER' : 'common.handlers.custom_exception_handler',
+    'EXCEPTION_HANDLER' : 'rest_framework.views.exception_handler',
+    'NON_FIELD_ERRORS_KEY' : 'non_field_errors',
 }
