95b9cfb468b64674380be06319cdee769426f637
RyoHatano <ryohatano0809@gmail.com>
2023-06-04 12:40:12 +0900

    An example of adding filtering (search) functionality to a view that inherits from ListAPIView

--- api/views.py
+++ api/views.py
@@ -1,9 +1,10 @@
 import logging
 
+from django_filters import rest_framework as filters
 from django.core.exceptions import PermissionDenied
 from django.http import Http404
 
-from rest_framework import viewsets, exceptions, status
+from rest_framework import viewsets, exceptions, status, generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
@@ -69,3 +70,7 @@ class BookViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
     
 
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [filters.DjangoFilterBackend]
